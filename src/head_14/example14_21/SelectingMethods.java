bb0a2471087db7b4e18369eb97a513e2f4f3b5bc
pasha <pasha_mugako@mail.ru>
2016-10-16 02:29:41 +0300

    New example 21 for head 14..

---
+++ src/head_14/example14_21/SelectingMethods.java
@@ -0,0 +1,47 @@
+package head_14.example14_21;
+
+import java.lang.reflect.*;
+import java.util.Date;
+
+class MethodSelector implements InvocationHandler {
+  private Object proxied;
+  public MethodSelector(Object proxied) {
+    this.proxied = proxied;
+  }
+  public Object
+  invoke(Object proxy, Method method, Object[] args)
+  throws Throwable {
+    if(method.getName().equals("interesting"))
+      System.out.println("Proxy detected the interesting method " + (new Date(System.currentTimeMillis())));
+    return method.invoke(proxied, args);
+  }
+}	
+
+interface SomeMethods {
+  void boring1();
+  void boring2();
+  void interesting(String arg);
+  void boring3();
+}
+
+class Implementation implements SomeMethods {
+  public void boring1() { System.out.println("boring1"); }
+  public void boring2() { System.out.println("boring2"); }
+  public void interesting(String arg) {
+    System.out.println("interesting " + arg);
+  }
+  public void boring3() { System.out.println("boring3"); }
+}	
+
+class SelectingMethods {
+  public static void main(String[] args) {
+    SomeMethods proxy= (SomeMethods)Proxy.newProxyInstance(
+      SomeMethods.class.getClassLoader(),
+      new Class[]{ SomeMethods.class },
+      new MethodSelector(new Implementation()));
+    proxy.boring1();
+    proxy.boring2();
+    proxy.interesting("bonobo");
+    proxy.boring3();
+  }
+}
