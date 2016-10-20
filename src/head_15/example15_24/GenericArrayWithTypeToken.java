219d7e6d427babe44894204b7ffe76e5735cb168
pasha <pasha_mugako@mail.ru>
2016-10-20 17:14:27 +0300

    Remake example 24 for head 15.

--- src/head_15/example15_24/GenericArrayWithTypeToken.java
+++ src/head_15/example15_24/GenericArrayWithTypeToken.java
@@ -2,22 +2,22 @@ package head_15.example15_24;
 import java.lang.reflect.*;
 
 public class GenericArrayWithTypeToken<T> {
-  private T[] array;
+    private T[] array;
+    @SuppressWarnings("unchecked")
+    public GenericArrayWithTypeToken(Class<T> type, int sz) {
+      array = (T[])Array.newInstance(type, sz);
+    }
 
-  public GenericArrayWithTypeToken(Class<T> type, int sz) {
-    array = (T[])Array.newInstance(type, sz);
-  }
+    public void put(int index, T item) {
+      array[index] = item;
+    }
 
-  public void put(int index, T item) {
-    array[index] = item;
-  }
+    public T get(int index) { return array[index]; }
 
-  public T get(int index) { return array[index]; }
+    public T[] rep() { return array; }
 
-  public T[] rep() { return array; }
-
-  public static void main(String[] args) {
-      GenericArrayWithTypeToken<Integer> gai = new GenericArrayWithTypeToken<Integer>(Integer.class, 10);
-      Integer[] ia = gai.rep();
-  }
-} ///:~
+    public static void main(String[] args) {
+        GenericArrayWithTypeToken<Integer> gai = new GenericArrayWithTypeToken<Integer>(Integer.class, 10);
+        Integer[] ia = gai.rep();
+    }
+}
