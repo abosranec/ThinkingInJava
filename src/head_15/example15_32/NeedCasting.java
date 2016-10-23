f3e630adf79d18ea92d8937b2354cf1c762a6ad9
pasha <pasha_mugako@mail.ru>
2016-10-23 23:32:09 +0300

    New example 32,33 for head 15.

---
+++ src/head_15/example15_32/NeedCasting.java
@@ -0,0 +1,10 @@
+package head_15.example15_32;
+import java.io.*;
+import java.util.*;
+
+class Widget{}
+
+public class NeedCasting<T,W> {
+  public void f(List<T> list){}
+  //public void f(List<W> list){}
+} ///:~
