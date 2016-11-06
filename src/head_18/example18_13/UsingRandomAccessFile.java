12d2a5d4d09bdd10db778419564a3f042e122a42
pasha <pasha_mugako@mail.ru>
2016-11-06 23:43:22 +0300

    Example 13 for head 18.

---
+++ src/head_18/example18_13/UsingRandomAccessFile.java
@@ -0,0 +1,28 @@
+package head_18.example18_13;
+import java.io.*;
+
+public class UsingRandomAccessFile {
+  static String file = "src\\head_18\\example18_13\\WorkClass.out";
+  static void display() throws IOException {
+    RandomAccessFile rf = new RandomAccessFile(file, "r");
+    for(int i = 0; i < 7; i++)
+      System.out.println(
+        "Value " + i + ": " + rf.readDouble());
+    System.out.println(rf.readUTF());
+    rf.close();
+  }
+  public static void main(String[] args)
+  throws IOException {
+    RandomAccessFile rf = new RandomAccessFile(file, "rw");
+    for(int i = 0; i < 7; i++)
+      rf.writeDouble(i*1.414);
+    rf.writeUTF("The end of the file");
+    rf.close();
+    display();
+    rf = new RandomAccessFile(file, "rw");
+    rf.seek(5*8);
+    rf.writeDouble(47.0001);
+    rf.close();
+    display();
+  }
+}
