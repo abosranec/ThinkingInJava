15f6b2d5d5f6a770f7c8c1bfefb385c5b3f0923c
pasha <pasha_mugako@mail.ru>
2016-11-10 01:39:27 +0300

    Example 22 for head 18.

---
+++ src/head_18/example18_22/WorkClass.java
@@ -0,0 +1,10 @@
+package head_18.example18_22;
+
+import java.io.PrintWriter;
+
+public class WorkClass {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out, true);
+        pw.println("hello");
+    }
+}
