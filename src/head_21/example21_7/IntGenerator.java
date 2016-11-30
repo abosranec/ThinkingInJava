92609b1f3f9e41b2e4b928e7d300d73afad65486
pasha <pasha_mugako@mail.ru>
2016-12-01 01:12:25 +0300

    New example 7 for head 21.

---
+++ src/head_21/example21_7/IntGenerator.java
@@ -0,0 +1,9 @@
+package head_21.example21_7;
+
+public abstract class IntGenerator {
+  private volatile boolean canceled = false;
+  public abstract int next();
+  // Allow this to be canceled:
+  public void cancel() { canceled = true; }
+  public boolean isCanceled() { return canceled; }
+}
