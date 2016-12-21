f9c42dc196dd88cd540935026a5e104f56715bce
pasha <pasha_mugako@mail.ru>
2016-12-21 17:20:46 +0300

    Example 3 for head 22.

---
+++ src/head_22/example22_3/CatchAction.java
@@ -0,0 +1,40 @@
+package head_22.example22_3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import head_22.example22_2.SwingConsole;
+
+public class CatchAction extends JFrame {
+    private JButton b1 = new JButton("button 1"),
+            b2 = new JButton("button 2");
+    private JTextField tf = new JTextField(10);
+    private JTextArea ta = new JTextArea();
+    private int counter = 0;
+
+    private class ButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = ((JButton)e.getSource()).getText();
+            tf.setText(name);
+            ta.append(counter++ + ": " + name + "\n");
+        }
+    }
+
+    private ButtonListener bl = new ButtonListener();
+
+    public CatchAction() {
+        b1.addActionListener(bl);
+        b2.addActionListener(bl);
+        setLayout(new FlowLayout());
+        add(b1);
+        add(b2);
+        add(tf);
+        add(ta);
+    }
+
+    public static void main(String[] args) {
+        SwingConsole.run(new CatchAction(),300,300);
+    }
+}
